6917b90c24511a9922d47a9ea8b15b6deea71d45
myakhontov <michael.yakhontov23@sitechhs.com>
2020-04-29 16:32:36 -0400

    finished startGame functions

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log("hello")
+const game = ()=> {
+    let pScore = 0
+    let cScore = 0
+
+    //start the game
+    const startGame = () =>{
+        const playBtn = document.querySelector('.intro button')
+        const introScreen = document.querySelector('.intro')
+        const match = document.querySelector('.match')
+
+        playBtn.addEventListener('click', () =>{
+            introScreen.classList.add('fadeOut')
+            match.classList.add('fadeIn')
+        })
+    }
+    //call all inner functions
+    startGame()
+}
+
+//start the game func
+game()
+
+/////////////////up to 24:44 in Dev Ed video
